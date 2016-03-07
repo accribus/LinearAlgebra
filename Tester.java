377d3935a47726e69f08ea89a52470fe2e64f69a
Chris <accribus@users.noreply.github.com>
2016-03-07 14:58:23 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,19 +4,29 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int d = 5;
-		int scal = 2;
+		int scal = 5;
 		VectorLinAlg vector = new VectorLinAlg(d);
-		System.out.println(vector.showVector());
-		vector.scalar(vector, scal);
-		System.out.println(vector.showVector());
+		System.out.println("vector" + vector.toString());
+		vector = vector.scalar(vector, scal);
+		System.out.println("vector" + vector.toString());
 		
 		
 		double dd = 5.0;
 		
 		VectorLinAlg vectorDouble = new VectorLinAlg(dd);
-		System.out.println(vectorDouble.showVector());
-		vector.scalar(vectorDouble, scal);
-		System.out.println(vectorDouble.showVector());
+		System.out.println("vectorDouble" + vectorDouble.toString());
+		vectorDouble = VectorLinAlg.scalar(vectorDouble, scal);
+		System.out.println("vectorDouble" + vectorDouble.toString());
+                
+                
+                VectorLinAlg vecDblTmp = new VectorLinAlg(vectorDouble.getDimension(),2.0);
+                VectorLinAlg.scalar(vecDblTmp, 5);
+                System.out.println("vecDblTmp" + vecDblTmp.toString());
+                vectorDouble = VectorLinAlg.subtractVectors(vecDblTmp, vectorDouble);
+                System.out.println("subtracting vectors " + vectorDouble.toString());
+                
+                VectorLinAlg.zeroVector(vectorDouble);
+                System.out.println(vectorDouble.toString());
 
 	}
 
