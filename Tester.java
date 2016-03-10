341940af9b7269f9cdc4fc0b4178a0079f19988f
Chris <accribus@users.noreply.github.com>
2016-03-10 15:34:07 -0500

    Added files via upload
    made changes so that only doubles are used.

--- Tester.java
+++ Tester.java
@@ -8,34 +8,22 @@ public class Tester {
             double[] seedDbl1 = new double[]{1.0,2.0,3.0};
             double[] seedDbl2 = new double[]{4.0,5.0,6.0};
 
-            VectorLinAlg intV1 = new VectorLinAlg(seedInt1);
-            VectorLinAlg intV2 = new VectorLinAlg(seedInt2);
             VectorLinAlg dblV1 = new VectorLinAlg(seedDbl1);
             VectorLinAlg dblV2 = new VectorLinAlg(seedDbl2);
 
-            System.out.println("intV1" + intV1.toString());
-            System.out.println("intV2" + intV2.toString());
             System.out.println("dblV1" + dblV1.toString());
             System.out.println("dblV2" + dblV2.toString());
 
-            System.out.println("intV1 vector magnitude: " + VectorLinAlg.getMagnitude(intV1));
-            System.out.println("intV2 vector magnitude: " + VectorLinAlg.getMagnitude(intV2));
             System.out.println("dblV1 vector magnitude: " + VectorLinAlg.getMagnitude(dblV1));
             System.out.println("dblV2 vector magnitude: " + VectorLinAlg.getMagnitude(dblV2));
 
-            double intDot = VectorLinAlg.dotProduct(intV1, intV2);
             double dblDot = VectorLinAlg.dotProduct(dblV1, dblV2);
 
-            System.out.println("int vector dot product: " + intDot);
             System.out.println("dbl vector dot product: " + dblDot);
-            
-            System.out.println("getTheta(intV1, intV2): " + VectorLinAlg.getTheta(intV1, intV2));
-            
-            VectorLinAlg vecCrossPrdInt = VectorLinAlg.crossProduct(intV1, intV2);
-            System.out.println("vecCrossPrdInt: " + vecCrossPrdInt.toString());
             
             VectorLinAlg vecCrossPrdDbl = VectorLinAlg.crossProduct(dblV1, dblV2);
             System.out.println("vecCrossPrdDbl: " + vecCrossPrdDbl.toString());
+            
 
 	}
 
--- VectorLinAlg.java
+++ VectorLinAlg.java
@@ -7,75 +7,43 @@
  */
 public class VectorLinAlg {
 	
-	
-	private int[] vectorInt;
 	private double[] vectorDouble;
-	private String type = "";
         	
 	/**
-         * creates a vector with random int values ranging from 0 to 10 and dimension = dim
-         * if an int is used for dim, then the vector values will be double
-         * @param dim 
+         * 
          */
-	public VectorLinAlg(int dim){
-		type = "int";
-		this.vectorInt = new int[dim];
-		for(int i = 0; i < this.vectorInt.length; i++){
-			this.vectorInt[i] = (int)Math.floor(Math.random()*10);
-		}
-	}
+        public VectorLinAlg(){
+            
+        }
+        
 	
         /**
          * creates a vector with random int values ranging from 0 to 10 and dimension = dim
          * if a double is used for dim, then the vector values will be double
          * @param dim 
          */
-	public VectorLinAlg(double dim){
-		type = "double";
+	public VectorLinAlg(int dim){
+            
 		this.vectorDouble = new double[(int)dim];
 		for(int i = 0; i < this.vectorDouble.length; i++){
 			this.vectorDouble[i] = Math.random()*10; 
 		}
 	}
         
-        /**
-         * takes in an int[] an wraps it in a VectorLinAlg object
-         * @param arr 
-         */
-	public VectorLinAlg(int[] arr){
-		type = "int";
-		this.vectorInt = new int[arr.length];
-		for(int i = 0; i < arr.length; i++){
-                    this.vectorInt[i] = arr[i];
-		}
-	}
-	
+        	
         /**
          * takes in a double[] an wraps it in a VectorLinAlg object
          * @param arr 
          */
 	public VectorLinAlg(double[] arr){
-		type = "double";
+		
 		this.vectorDouble = new double[arr.length];
 		for(int i = 0; i < arr.length; i++){
 			this.vectorDouble[i] = arr[i];
 		}
 	}
 	
-	
-        /**
-         * creates a vector with random int values ranging from 0 to range and dimension = dim
-         * @param dim
-         * @param range 
-         */
-	public VectorLinAlg(int dim, int range){
-		type = "int";
-		this.vectorInt = new int[dim];
-		for(int i = 0; i < this.vectorInt.length; i++){
-			this.vectorInt[i] = (int)Math.floor(Math.random()*range);
-		}
-	}
-	
+		
 	
         /**
          * creates a vector with random double values ranging from 0 to range and dimension = dim
@@ -83,35 +51,20 @@ public class VectorLinAlg {
          * @param range 
          */
 	public VectorLinAlg(int dim, double range){
-		type = "double";
+		
 		this.vectorDouble = new double[dim];
 		for(int i = 0; i < this.vectorDouble.length; i++){
 			this.vectorDouble[i] = Math.random()*range; 
 		}
 	}
-	
-        /**
-         * 
-         * @return type of vector (int or double)
-         */
-	public String getType(){
-		return type;
-	}
-	
+		
         /**
          * 
          * @return the dimension of the vector (count of elements, example: [3, 4, 5] returns 3 
          */
 	public int getDimension(){
-		int dimension = 0;
-		
-		if(getType().equals("int")){
-			dimension = vectorInt.length;
-		}else{
-			dimension = vectorDouble.length;
-		}
-		
-		return dimension;
+			
+		return vectorDouble.length;
 	}
 	
         /**
@@ -123,59 +76,36 @@ public class VectorLinAlg {
 	}
 	//used by overridden toString()
 	private String showVector(){
-		String res = "[";
-		if(this.getType().equals("int")){
-			for(int i = 0; i < this.getDimension(); i++){
-				if(i == 0){
-					res += this.getIndexAtVectorInt(i);
-				}else{
-					res += "," + this.getIndexAtVectorInt(i);
-				}
-			}
-		}else{
-			for(int i = 0; i < this.getDimension(); i++){
-				if(i == 0){
-					res += this.getIndexAtVectorDouble(i);
-				}else{
-					res += "," + this.getIndexAtVectorDouble(i);
-				}
-			}
-		}
 		
-		return res + "]";
+            String res = "<";
+                
+            for(int i = 0; i < this.getDimension(); i++){
+                if(i == 0){
+                        res += this.getIndexAtVector(i);
+                }else{
+                        res += "," + this.getIndexAtVector(i);
+                }
+            }
+
+            return res + ">";
 	}
 	
-        /**
-         * @see return value at index i
-         * @param i
-         * @return value at index i 
-         */
-	public int getIndexAtVectorInt(int i){
-		return this.vectorInt[i];
-	}
 	 /**
          * @see return value at index i
          * @param i
          * @return value at index i 
          */
-	public double getIndexAtVectorDouble(int i){
+	public double getIndexAtVector(int i){
 		return this.vectorDouble[i];
 	}
-	 /**
-         * @see set value at index i
-         * @param i
-         * @return void 
-         */
-	public void setIndexAtVectorInt(int i, int val){
-		this.vectorInt[i] = val;
-	}
+	 
 	
         /**
          * @see set value at index i
          * @param i
          * @return void 
          */
-	public void setIndexAtVectorDouble(int i, double val){
+	public void setIndexAtVector(int i, double val){
 		this.vectorDouble[i] = val;
 	}
 	
@@ -187,14 +117,8 @@ public class VectorLinAlg {
          */
 	public static VectorLinAlg scalar(VectorLinAlg vector, double scalar){
 		
-		if(vector.getType().equals("int")){
-			for(int i = 0; i < vector.getDimension(); i++){
-				vector.setIndexAtVectorInt(i, (int)scalar * vector.getIndexAtVectorInt(i));
-			}
-		} else{
-                    for(int i = 0; i < vector.getDimension(); i++){
-				vector.setIndexAtVectorDouble(i, scalar * vector.getIndexAtVectorDouble(i));
-			}                    
+		for(int i = 0; i < vector.getDimension(); i++){
+                    vector.setIndexAtVector(i, scalar * vector.getIndexAtVector(i));
                 }
 		
 		return vector;
@@ -208,23 +132,15 @@ public class VectorLinAlg {
          */
         public static VectorLinAlg addVectors(VectorLinAlg v1, VectorLinAlg v2){
             int dim = v1.getDimension();
-            String type = v1.getType();
+            
             VectorLinAlg result;
             
-            if(type.equals("int")){
-                int[] tmp = new int[dim];
-                for(int i = 0; i < dim; i++){
-                    tmp[i] = v1.getIndexAtVectorInt(i) + v2.getIndexAtVectorInt(i);
-                }
-                result = new VectorLinAlg(tmp);
-            } else{
-                double[] tmp = new double[dim];
-                for(int i = 0; i < dim; i++){
-                    tmp[i] = v1.getIndexAtVectorDouble(i) + v2.getIndexAtVectorDouble(i);
-                }
-                result = new VectorLinAlg(tmp);
+            double[] tmp = new double[dim];
+            for(int i = 0; i < dim; i++){
+                tmp[i] = v1.getIndexAtVector(i) + v2.getIndexAtVector(i);
             }
-            
+                        
+            result = new VectorLinAlg(tmp);
             return result;
         }
         
@@ -237,26 +153,17 @@ public class VectorLinAlg {
          */
         public static VectorLinAlg subtractVectors(VectorLinAlg v1, VectorLinAlg v2){
             int dim = v1.getDimension();
-            String type = v1.getType();
             
             System.out.print(v1.toString() + " - " + v2.toString());
             
             VectorLinAlg result;
             
-            if(type.equals("int")){
-                int[] tmp = new int[dim];
-                for(int i = 0; i < dim; i++){
-                    tmp[i] = v1.getIndexAtVectorInt(i) - v2.getIndexAtVectorInt(i);
-                }
-                result = new VectorLinAlg(tmp);
-            } else{
-                double[] tmp = new double[dim];
-                for(int i = 0; i < dim; i++){
-                    tmp[i] = v1.getIndexAtVectorDouble(i) - v2.getIndexAtVectorDouble(i);
-                }
-                result = new VectorLinAlg(tmp);
+            double[] tmp = new double[dim];
+            for(int i = 0; i < dim; i++){
+                tmp[i] = v1.getIndexAtVector(i) - v2.getIndexAtVector(i);
             }
             
+            result = new VectorLinAlg(tmp);
             return result;
         }
         
@@ -268,17 +175,10 @@ public class VectorLinAlg {
          */
         public static double dotProduct(VectorLinAlg v1, VectorLinAlg v2){
             
-            String type = v1.getType();
             int dim = v1.getDimension();
             double scalar = 0;
-            if(type.equals("int")){
-                for(int i = 0; i < dim; i++){
-                    scalar += v1.getIndexAtVectorInt(i) * v2.getIndexAtVectorInt(i);
-                }
-            } else{
-                for(int i = 0; i < dim; i++){
-                    scalar += v1.getIndexAtVectorDouble(i) * v2.getIndexAtVectorDouble(i);
-                }
+            for(int i = 0; i < dim; i++){
+                scalar += v1.getIndexAtVector(i) * v2.getIndexAtVector(i);
             }
             
             return scalar;
@@ -291,19 +191,12 @@ public class VectorLinAlg {
          */
         public static double getMagnitude(VectorLinAlg v){
             
-            String type = v.getType();
             int dim = v.getDimension();
             double squares = 0;
             double result = 0;
                     
-            if(type.equals("int")){
-                for(int i = 0; i < dim; i++){
-                    squares += Math.pow(v.getIndexAtVectorInt(i),2);
-                }
-            } else{
-                for(int i = 0; i < dim; i++){
-                    squares += Math.pow(v.getIndexAtVectorDouble(i),2);
-                }
+            for(int i = 0; i < dim; i++){
+                squares += Math.pow(v.getIndexAtVector(i),2);
             }
             
             result = Math.sqrt(squares);
@@ -336,7 +229,6 @@ public class VectorLinAlg {
          */
         public static VectorLinAlg crossProduct(VectorLinAlg v1, VectorLinAlg v2){
             
-            String type = v1.getType();
             int dim = v1.getDimension();
             int dim2 = v2.getDimension();
             if(dim != 3 | dim2 != 3){
@@ -345,25 +237,12 @@ public class VectorLinAlg {
             
             VectorLinAlg resultVector;
             
-            if(type.equals("int")){
-                
-                int a, b, c;                
-                a = v1.getIndexAtVectorInt(1) * v2.getIndexAtVectorInt(2) - v1.getIndexAtVectorInt(2) * v2.getIndexAtVectorInt(1); //a2b3 - a3b2
-                b = v1.getIndexAtVectorInt(2) * v2.getIndexAtVectorInt(0) - v1.getIndexAtVectorInt(0) * v2.getIndexAtVectorInt(2); //a3b1 - a1b3
-                c = v1.getIndexAtVectorInt(0) * v2.getIndexAtVectorInt(1) - v1.getIndexAtVectorInt(1) * v2.getIndexAtVectorInt(0); //a1b2 = a2b1
-                int[] vecValsInt = new int[]{a, b, c};
-                resultVector = new VectorLinAlg(vecValsInt);
-                
-            } else {
-                
-                double a, b, c;                
-                a = v1.getIndexAtVectorDouble(1) * v2.getIndexAtVectorDouble(2) - v1.getIndexAtVectorDouble(2) * v2.getIndexAtVectorDouble(1); //a2b3 - a3b2
-                b = v1.getIndexAtVectorDouble(2) * v2.getIndexAtVectorDouble(0) - v1.getIndexAtVectorDouble(0) * v2.getIndexAtVectorDouble(2); //a3b1 - a1b3
-                c = v1.getIndexAtVectorDouble(0) * v2.getIndexAtVectorDouble(1) - v1.getIndexAtVectorDouble(1) * v2.getIndexAtVectorDouble(0); //a1b2 = a2b1
-                double[] vecValsDbl = new double[]{a, b, c};
-                resultVector = new VectorLinAlg(vecValsDbl);
-                
-            }
+            double a, b, c;                
+            a = v1.getIndexAtVector(1) * v2.getIndexAtVector(2) - v1.getIndexAtVector(2) * v2.getIndexAtVector(1); //a2b3 - a3b2
+            b = v1.getIndexAtVector(2) * v2.getIndexAtVector(0) - v1.getIndexAtVector(0) * v2.getIndexAtVector(2); //a3b1 - a1b3
+            c = v1.getIndexAtVector(0) * v2.getIndexAtVector(1) - v1.getIndexAtVector(1) * v2.getIndexAtVector(0); //a1b2 = a2b1
+            double[] vecValsDbl = new double[]{a, b, c};
+            resultVector = new VectorLinAlg(vecValsDbl);
             
             return resultVector;
         }
@@ -374,17 +253,10 @@ public class VectorLinAlg {
          */
         public static void zeroVector(VectorLinAlg v){
             
-            String type = v.getType();
             int dim = v.getDimension();
             
-            if(type.equals("int")){
-                for(int i = 0; i < dim; i++){
-                    v.setIndexAtVectorInt(i, 0);
-                }
-            }else{
-                for(int i = 0; i < dim; i++){
-                    v.setIndexAtVectorDouble(i, 0.0);
-                }
+            for(int i = 0; i < dim; i++){
+                v.setIndexAtVector(i, 0);
             }
         }
 
